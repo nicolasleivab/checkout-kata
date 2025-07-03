0d594123cffbe290dc363abd9b4b1e62849e6ab2
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2025-07-04 00:52:54 +0200

    style: reset css and width consistency

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import Checkout from "./presentation/views/Checkout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./presentation/styles/reset.css";
 
 const qc = new QueryClient();
 const root = document.getElementById("root")!;
